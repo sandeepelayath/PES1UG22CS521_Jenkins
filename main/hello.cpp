dd20ef8d7e12f5af669d1003bd030cfc20b50e65
Sandeep Elayath <sandeepelayath@gmail.com>
2025-03-18 22:20:15 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,10 +1,7 @@
-//File: hello.cpp
 #include <iostream>
 using namespace std;
 
-int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this: Updated by Sandeep  \n";
-  return 0;
+int main() {
+    cout << "Hello, Jenkins Pipeline!" << endl;
+    return 0;
 }
